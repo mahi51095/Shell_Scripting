42f6d686a94fc45c477616c9fa702dd8f9f49859
Mahesh <mahilingabathula@gmail.com>
2025-02-05 20:15:36 +0530

    Message

--- 3_var.sh
+++ 3_var.sh
@@ -4,3 +4,9 @@ echo "Please enter your name :"
 read USERNAME
 
 echo "Username entered is: $USERNAME"
+
+
+echo "Please enter your Password :"
+read PASSWORD
+
+echo "Username entered is: $PASSWORD"
