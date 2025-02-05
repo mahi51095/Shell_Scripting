64c140b1e67ae5089512c60420904d94d053edfb
Mahesh <mahilingabathula@gmail.com>
2025-02-05 13:33:38 +0530

    Script

--- Hello-world.sh
+++ Hello-world.sh
@@ -4,5 +4,5 @@ Person1=Ramesh
 Person2=Mahesh
 Person3=Suresh
 
-echo "$Person2 : Hi $Person2, How are you"
-echo "$Person2: Im good. $Person2, I met $Person3 "
+echo "$Person1 : Hi $Person2, How are you"
+echo "$Person2: Im good. $Person1, I met $Person3 "
