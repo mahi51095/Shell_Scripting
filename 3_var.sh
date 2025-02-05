13b4b7512cff239b15522e50fa90da686cb15730
Mahesh <mahilingabathula@gmail.com>
2025-02-05 20:16:55 +0530

    Message

--- 3_var.sh
+++ 3_var.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Please enter your name :"
-read USERNAME
+read -s USERNAME
 
 echo "Username entered is: $USERNAME"
 
 
 echo "Please enter your Password :"
-read PASSWORD
+read -s PASSWORD
 
 echo "Username entered is: $PASSWORD"
